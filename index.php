f4b7a9a318f2afc83e4f86b33a1567fa771d1e7e
ryampeguero <ryampeguero@gmail.com>
2024-05-22 15:04:31 +0200

    feat: first milestone added

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+$email_array = ["ryampeguero@gmail.com", "mariorossi@gmail.com", "ilariaverdi@hotmail.it"];
+
+
+?>
 <html lang="en">
 
 <head>
@@ -7,7 +12,37 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
+
 <body>
-    <h2>ciao</h2>
+    <div class="container mt-5">
+        <?php if (!isset($_GET["email"])) {
+        ?>
+            <div class="row d-flex justify-content-center">
+                <div class="col-8 border">
+                    <form>
+                        <div class="mb-3">
+                            <label for="myEmail" class="form-label">Inserire Email</label>
+                            <input type="email" class="form-control" id="myEmail" name="email" placeholder="mariorossi@gmail.com">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+            <?php
+        } else {
+            $user_mail = $_GET["email"];
+
+            if (in_array($user_mail, $email_array)) {
+                session_start();
+            ?>
+                <div class="row">
+                    <h4><?php echo "Benvenuto utente: " . $user_mail;?></h4>
+                </div>
+        <?php
+            }
+        }
+        ?>
+    </div>
 </body>
+
 </html>
